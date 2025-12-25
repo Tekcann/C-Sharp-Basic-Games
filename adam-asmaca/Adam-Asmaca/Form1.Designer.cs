9a100e6f845c0ba2e8d415a489570cd7a8dbdfec
Tekcann <ahmetsahintekcan@gmail.com>
2025-12-25 17:09:56 +0300

    new game added 25.12.2025

---
+++ adam-asmaca/Adam-Asmaca/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Adam_Asmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelHarf0 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonEXİT = new System.Windows.Forms.Button();
+            this.buttonSTART = new System.Windows.Forms.Button();
+            this.labelHarf1 = new System.Windows.Forms.Label();
+            this.labelHarf2 = new System.Windows.Forms.Label();
+            this.labelHarf3 = new System.Windows.Forms.Label();
+            this.labelHarf4 = new System.Windows.Forms.Label();
+            this.labelHarf5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBoxBosHarf = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Adam_Asmaca.Properties.Resources._0_hata;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(410, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelHarf0
+            // 
+            this.labelHarf0.AutoSize = true;
+            this.labelHarf0.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelHarf0.Location = new System.Drawing.Point(44, 465);
+            this.labelHarf0.Name = "labelHarf0";
+            this.labelHarf0.Size = new System.Drawing.Size(40, 39);
+            this.labelHarf0.TabIndex = 1;
+            this.labelHarf0.Text = "X";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Adam_Asmaca.Properties.Resources.boslukluCizgi;
+            this.pictureBox2.Location = new System.Drawing.Point(38, 507);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(324, 13);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // buttonEXİT
+            // 
+            this.buttonEXİT.BackColor = System.Drawing.Color.Brown;
+            this.buttonEXİT.Location = new System.Drawing.Point(12, 556);
+            this.buttonEXİT.Name = "buttonEXİT";
+            this.buttonEXİT.Size = new System.Drawing.Size(141, 47);
+            this.buttonEXİT.TabIndex = 3;
+            this.buttonEXİT.Text = "EXİT";
+            this.buttonEXİT.UseVisualStyleBackColor = false;
+            this.buttonEXİT.Click += new System.EventHandler(this.buttonEXİT_Click);
+            // 
+            // buttonSTART
+            // 
+            this.buttonSTART.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.buttonSTART.Location = new System.Drawing.Point(159, 556);
+            this.buttonSTART.Name = "buttonSTART";
+            this.buttonSTART.Size = new System.Drawing.Size(141, 47);
+            this.buttonSTART.TabIndex = 3;
+            this.buttonSTART.Text = "START";
+            this.buttonSTART.UseVisualStyleBackColor = false;
+            this.buttonSTART.Click += new System.EventHandler(this.buttonSTART_Click);
+            // 
+            // labelHarf1
+            // 
+            this.labelHarf1.AutoSize = true;
+            this.labelHarf1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelHarf1.Location = new System.Drawing.Point(98, 465);
+            this.labelHarf1.Name = "labelHarf1";
+            this.labelHarf1.Size = new System.Drawing.Size(40, 39);
+            this.labelHarf1.TabIndex = 1;
+            this.labelHarf1.Text = "X";
+            // 
+            // labelHarf2
+            // 
+            this.labelHarf2.AutoSize = true;
+            this.labelHarf2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelHarf2.Location = new System.Drawing.Point(152, 465);
+            this.labelHarf2.Name = "labelHarf2";
+            this.labelHarf2.Size = new System.Drawing.Size(40, 39);
+            this.labelHarf2.TabIndex = 1;
+            this.labelHarf2.Text = "X";
+            // 
+            // labelHarf3
+            // 
+            this.labelHarf3.AutoSize = true;
+            this.labelHarf3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelHarf3.Location = new System.Drawing.Point(206, 465);
+            this.labelHarf3.Name = "labelHarf3";
+            this.labelHarf3.Size = new System.Drawing.Size(40, 39);
+            this.labelHarf3.TabIndex = 1;
+            this.labelHarf3.Text = "X";
+            // 
+            // labelHarf4
+            // 
+            this.labelHarf4.AutoSize = true;
+            this.labelHarf4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelHarf4.Location = new System.Drawing.Point(260, 465);
+            this.labelHarf4.Name = "labelHarf4";
+            this.labelHarf4.Size = new System.Drawing.Size(40, 39);
+            this.labelHarf4.TabIndex = 1;
+            this.labelHarf4.Text = "X";
+            // 
+            // labelHarf5
+            // 
+            this.labelHarf5.AutoSize = true;
+            this.labelHarf5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelHarf5.Location = new System.Drawing.Point(314, 465);
+            this.labelHarf5.Name = "labelHarf5";
+            this.labelHarf5.Size = new System.Drawing.Size(40, 39);
+            this.labelHarf5.TabIndex = 1;
+            this.labelHarf5.Text = "X";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.YellowGreen;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "Ç",
+            "D",
+            "E",
+            "F",
+            "G",
+            "Ğ",
+            "H",
+            "I",
+            "İ",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "Ö",
+            "P",
+            "R",
+            "S",
+            "Ş",
+            "T",
+            "U",
+            "Ü",
+            "V",
+            "Y",
+            "Z"});
+            this.comboBox1.Location = new System.Drawing.Point(593, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(159, 37);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "HARFLER";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // listBoxBosHarf
+            // 
+            this.listBoxBosHarf.BackColor = System.Drawing.Color.Fuchsia;
+            this.listBoxBosHarf.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBoxBosHarf.FormattingEnabled = true;
+            this.listBoxBosHarf.ItemHeight = 29;
+            this.listBoxBosHarf.Location = new System.Drawing.Point(446, 12);
+            this.listBoxBosHarf.Name = "listBoxBosHarf";
+            this.listBoxBosHarf.Size = new System.Drawing.Size(141, 439);
+            this.listBoxBosHarf.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(800, 615);
+            this.Controls.Add(this.listBoxBosHarf);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonSTART);
+            this.Controls.Add(this.buttonEXİT);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.labelHarf5);
+            this.Controls.Add(this.labelHarf4);
+            this.Controls.Add(this.labelHarf3);
+            this.Controls.Add(this.labelHarf2);
+            this.Controls.Add(this.labelHarf1);
+            this.Controls.Add(this.labelHarf0);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelHarf0;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button buttonEXİT;
+        private System.Windows.Forms.Button buttonSTART;
+        private System.Windows.Forms.Label labelHarf1;
+        private System.Windows.Forms.Label labelHarf2;
+        private System.Windows.Forms.Label labelHarf3;
+        private System.Windows.Forms.Label labelHarf4;
+        private System.Windows.Forms.Label labelHarf5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBoxBosHarf;
+    }
+}
+
---
+++ adam-asmaca/Adam-Asmaca/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adam_Asmaca
+{
+    public partial class Form1 : Form
+    {// kullanılan kelimeler// 0         1          2        3        4         5         6          7        8        9
+        string[] words = { "MEHMET", "SERKAN", "BURHAN", "RIDVAN", "ZEYNEP", "MELİKE", "HATİCE", "TEOMAN", "NURHAN", "TARKAN"};
+        //Alfebedeki kelimler
+        string[] alphabet ={"A", "B", "C", "Ç", "D", "E", "F", "G", "Ğ", "H", "I", "İ", "J", "K", "L", "M", "N", "O", "Ö", "P", "R", "S", "Ş", "T", "U", "Ü", "V", "Y", "Z"};
+
+
+        Random rnd = new Random();//Random değer üretmek için kullanılan kod
+        
+        //değişkenler atanır
+        int rndWord, wrongs = 0;
+        string word,oneWord;
+        bool wordIsRight;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEXİT_Click(object sender, EventArgs e)
+        {
+            Application.Exit();//uygulamayı kapatır
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Yazılan kelşmenin doğru olup olmamasını kontrol eder.Doğruysa calışır
+            if (comboBox1.Text.Trim().ToUpper() == word.ToUpper().Trim())
+            {  //ekranda metin gösteirir
+                MessageBox.Show("Doğru Bildiniz.\n Tekrar Oynamak İçin Start Butonuna Bas", "MAŞALLAH");
+                basaDön();//metot çağırır
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i;//değişken oluşturur
+            wordIsRight = false;//değişkenin değeri yanlış olarak değiştirir
+
+            //kelmenin uzunluğu kadar içindeki kodları takrar çalıştırır
+            for (i = 0;i < word.Trim().Length; i++)
+            {
+                oneWord = word.ToUpper().Substring(i, 1);//kelimeden sırayla harfler alır
+                //harf ile comboBax ın metninin doğru olup olmamasını kontrol eder
+                if (comboBox1.Text == oneWord)
+                {
+                    wordIsRight = true;//değişkenin değerini doğru olarak değiştirir
+                    //i değişkeninin değerine göre kodlar çalıştırır
+                    switch (i) {
+                        case 0://i değişkeninin değeri 0 ise altındaki kodlar çalışır
+                            labelHarf0.Text = comboBox1.Text;//label a harf yazdırır
+                            break;//geri kalan komutları atlar
+
+                            //aynı mantık aşağıda ki kodlarda devam eder
+                        case 1:
+                            labelHarf1.Text = comboBox1.Text;
+                            break;
+                        case 2:
+                            labelHarf2.Text = comboBox1.Text;
+                            break;
+                        case 3:
+                            labelHarf3.Text = comboBox1.Text;
+                            break;
+                        case 4:
+                            labelHarf4.Text = comboBox1.Text;
+                            break;
+                        case 5:
+                            labelHarf5.Text = comboBox1.Text;
+                            break;
+                    }
+                    
+                }
+                
+            }
+            if (wordIsRight == false)//değişkenin değeri doğru ise içindeki komutları çalıştırır
+            {
+                switch (wrongs)//değişkenin içindeki değere göre komutlar çalıştırır
+                {
+                    case 0://değişkenin değeri 0 ise bu komutlar çalışır
+                        pictureBox1.Image = Properties.Resources._1_hata;//PictureBox ın resmini değiştirir
+                      //değişkenin değerini bit artırır; geri kalan komutları atlar
+                        wrongs++; break;
+                    //aynı mantık aşağıda ki kodlarda devam eder
+                    case 1:
+                        pictureBox1.Image = Properties.Resources._2_hata;
+                        wrongs++; break;
+                    case 2:
+                        pictureBox1.Image = Properties.Resources._3_hata;
+                        wrongs++; break;
+                    case 3:
+                        pictureBox1.Image = Properties.Resources._4_hata;
+                        wrongs++; break;
+                    case 4:
+                        pictureBox1.Image = Properties.Resources._5_hata;
+                        wrongs++; break;
+                    case 5:
+                        pictureBox1.Image = Properties.Resources._6_hata;
+                        wrongs++;break;
+                    
+                        
+                        
+
+                }
+
+            }
+
+            listBoxBosHarf.Items.Add(comboBox1.Text);//listBox a seçili olan harfi ekler
+            comboBox1.Items.RemoveAt(comboBox1.SelectedIndex);// seçili olan harfi comboBox ın listesinden siler
+
+            //değişkenin değeri 6 olduğunda içindeki komutları çalıştırır
+            if(wrongs == 6)
+            {  //ekranda metin gösterir
+                MessageBox.Show("Tekrar Oynamak İçin Start Butonuna Bas", "KAYBETTİN");
+
+                basaDön();//metotu çalıştırır
+               
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBoxBosHarf.Enabled = false;// listBox u kullanılmaz hale getirir
+            comboBox1.Enabled = false;// comboBox u kullanılmaz hale getirir
+
+            //ekranda metin gösterir
+            MessageBox.Show("Bu bir ADAM ASMACA Oyunudur.\n Kelimeler İnsan İsimleridir.\n ComboBoxtan İstediğiniz Harfi secebilir Ya da Üstüne İstediğiniz Kelimeyi Yazarak Deneyebilirsiniz.","ÖĞRETİCi");
+        }
+
+        private void buttonSTART_Click(object sender, EventArgs e)
+        {
+            comboBox1.Enabled = true;// listBox u kullanılabilir hale getirir
+            listBoxBosHarf.Enabled = true;// comboBox u kullanılabilir hale getirir
+
+
+            rndWord = rnd.Next(0, 10);//random değer üretir
+
+            word = words[rndWord];//değişkene dizilerden bir raskele kelime atar
+
+            MessageBox.Show("Kelime Seçildi");//ekranda metin göstirir
+
+            basaDön();//metodu çalıştırır
+
+
+            //label ların matinlerini değiştirir
+            labelHarf0.Text = "X";
+            labelHarf1.Text = "X";
+            labelHarf2.Text = "X";
+            labelHarf3.Text = "X";
+            labelHarf4.Text = "X";
+            labelHarf5.Text = "X";
+
+
+
+            #region boş kod
+            //Bu kodlar random word armanın farklı bir yoludur.
+            /*for(int i=0;i<10;i=i+1)
+            {
+                if (i == rndWord)
+                {
+                    string lazim = words[i];
+                    MessageBox.Show(words[i]);
+                }
+            }*/
+            #endregion
+        }
+        private void basaDön()
+        {
+
+            //lanel ların metinlerini değiştirir(seçilen kelimeyi ekrana yazılır
+            labelHarf0.Text = word.Trim().Substring(0, 1);
+            labelHarf1.Text = word.Trim().Substring(1, 1);
+            labelHarf2.Text = word.Trim().Substring(2, 1);
+            labelHarf3.Text = word.Trim().Substring(3, 1);
+            labelHarf4.Text = word.Trim().Substring(4, 1);
+            labelHarf5.Text = word.Trim().Substring(5, 1);
+
+            comboBox1.Items.Clear();//ComboBoxtaki harfleri siler
+            listBoxBosHarf.Items.Clear();//listBox un içinde yazılanları siler
+
+            //bir döngü başlatır doğru olduğu süre boyunca çalışır
+            for (int j = 0; j < 29; j++)
+            {
+                comboBox1.Items.Add(alphabet[j]);//comboBox a alfabeyi harf yazdırır
+            }
+
+            pictureBox1.Image = Properties.Resources._0_hata;//pictureBox ın resmini değiştirir
+            wrongs = 0;//değişkenin değerini 0 olarak ayarlar
+
+
+        }
+    }
+}
